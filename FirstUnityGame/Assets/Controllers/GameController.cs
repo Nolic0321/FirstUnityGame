e39a1bac1691f9c4c2ef098238357504edc67e3d
Nolic0321 <Nolic0321@gmail.com>
2016-04-29 13:27:51 -0500

    Got a small hallway

---
+++ FirstUnityGame/Assets/Controllers/GameController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+    public GameObject player;
+    public float speed;
+    // Use this for initialization
+    void Start () {
+        speed = .05F;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        player.transform.Translate(new Vector3(0, 0, 1*speed));
+	}
+}
